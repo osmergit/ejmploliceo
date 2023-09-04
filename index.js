b6e0d78a876ff0e479fb5fd1e8455040915e816e
Osmer Eduardo Ruiz Mateus <osmer1976@gmail.com>
2023-09-04 15:33:14 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import  express from "express"
+import cors from 'cors';
+import { router } from "./routes/routes.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/usuarios',router);
+
+ 
+
+app.listen(8000, ()=> {
+    console.log('Servidor 🚀🚀Corriendo en  http://localhost:8000/')
+})
+
+
+
